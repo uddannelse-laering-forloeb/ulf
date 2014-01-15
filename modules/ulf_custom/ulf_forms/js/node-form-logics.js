fb0433c28138a3cc73609b009b5d14ff3f944f15
Martin Yde <my@aarhus.dk>
2014-01-15 12:14:25 +0100

    Changed node form to consider subjects

--- modules/ulf_custom/ulf_forms/js/node-form-logics.js
+++ modules/ulf_custom/ulf_forms/js/node-form-logics.js
@@ -1,6 +1,8 @@
 /**
  * Logics for the node form
  *
+ * Alot of fields change due to the selection of the target group field.
+ *
  */
 
 (function($) {
@@ -12,17 +14,17 @@
     switch (selection) {
       // Preschool id.
       case '3':
-        selection_preschool();
+        selectionPreschool();
         break;
 
-      // School id.
+      // Primary school id.
       case '34':
-        selection_school();
+        selectionPrimarySchool();
         break;
 
       // Youth education id.
       case '40':
-        selection_youth();
+        selectionYouth();
         break;
     }
 
@@ -32,25 +34,27 @@
       switch (selection) {
         // Preschool id.
         case '3':
-          selection_preschool();
+          selectionPreschool();
           break;
 
         // School id.
         case '34':
-          selection_school();
+          selectionPrimarySchool();
           break;
 
         // Youth education id.
         case '40':
-          selection_youth();
+          selectionYouth();
           break;
       }
     });
   });
 
   // Dagtilbud selected.
-  function selection_preschool() {
+  function selectionPreschool() {
     // The sub target group field selection values.
+
+    // Preschool selections
     $( ".form-item-field-target-group-sub-und-6" ).show();
     $( ".form-item-field-target-group-sub-und-9" ).show();
     $( ".form-item-field-target-group-sub-und-16" ).show();
@@ -59,6 +63,7 @@
     $( ".form-item-field-target-group-sub-und-35" ).show();
     $( ".form-item-field-target-group-sub-und-31" ).show();
 
+    // Primary school selections
     $( ".form-item-field-target-group-sub-und-4" ).hide();
     $( ".form-item-field-target-group-sub-und-25" ).hide();
     $( ".form-item-field-target-group-sub-und-5" ).hide();
@@ -71,6 +76,7 @@
     $( ".form-item-field-target-group-sub-und-37" ).hide();
     $( ".form-item-field-target-group-sub-und-13" ).hide();
 
+    // Youth selections
     $( ".form-item-field-target-group-sub-und-14" ).hide();
     $( ".form-item-field-target-group-sub-und-32" ).hide();
     $( ".form-item-field-target-group-sub-und-2" ).hide();
@@ -86,11 +92,16 @@
     // We act on our own classes as well. This is usefull for changing help texts (See purpose field for example).
     $( ".is-preschool" ).show();
     $( ".is-school" ).hide();
+
+    // The happenings of the subjectfield gets its very own function to maintain a bit of order.
+    subjectSelection('preschool');
   }
 
   // Grundskole selected
-  function selection_school() {
+  function selectionPrimarySchool() {
     // The sub target group field selection values.
+
+    // Preschool selections
     $( ".form-item-field-target-group-sub-und-6" ).hide();
     $( ".form-item-field-target-group-sub-und-9" ).hide();
     $( ".form-item-field-target-group-sub-und-16" ).hide();
@@ -99,6 +110,7 @@
     $( ".form-item-field-target-group-sub-und-35" ).hide();
     $( ".form-item-field-target-group-sub-und-31" ).hide();
 
+    // Primary school selections
     $( ".form-item-field-target-group-sub-und-4" ).show();
     $( ".form-item-field-target-group-sub-und-25" ).show();
     $( ".form-item-field-target-group-sub-und-5" ).show();
@@ -111,6 +123,7 @@
     $( ".form-item-field-target-group-sub-und-37" ).show();
     $( ".form-item-field-target-group-sub-und-13" ).show();
 
+    // Youth selections
     $( ".form-item-field-target-group-sub-und-14" ).hide();
     $( ".form-item-field-target-group-sub-und-32" ).hide();
     $( ".form-item-field-target-group-sub-und-2" ).hide();
@@ -126,11 +139,16 @@
     // We act on our own classes as well. This is usefull for changing help texts (See purpose field for example).
     $( ".is-preschool" ).hide();
     $( ".is-school" ).show();
+
+    // The happenings of the subject field gets its very own function to maintain a bit of order.
+    subjectSelection('primary_school');
   }
 
   // Ungdomsuddannelse selected.
-  function selection_youth() {
+  function selectionYouth() {
     // The sub target group field selection values.
+
+    // Preschool selections
     $( ".form-item-field-target-group-sub-und-6" ).hide();
     $( ".form-item-field-target-group-sub-und-9" ).hide();
     $( ".form-item-field-target-group-sub-und-16" ).hide();
@@ -139,6 +157,7 @@
     $( ".form-item-field-target-group-sub-und-35" ).hide();
     $( ".form-item-field-target-group-sub-und-31" ).hide();
 
+    // Primary school selections
     $( ".form-item-field-target-group-sub-und-4" ).hide();
     $( ".form-item-field-target-group-sub-und-25" ).hide();
     $( ".form-item-field-target-group-sub-und-5" ).hide();
@@ -151,6 +170,7 @@
     $( ".form-item-field-target-group-sub-und-37" ).hide();
     $( ".form-item-field-target-group-sub-und-13" ).hide();
 
+    // Youth selections
     $( ".form-item-field-target-group-sub-und-14" ).show();
     $( ".form-item-field-target-group-sub-und-32" ).show();
     $( ".form-item-field-target-group-sub-und-2" ).show();
@@ -166,5 +186,101 @@
     // We act on our own classes as well. This is usefull for changing help texts (See purpose field for example).
     $( ".is-preschool" ).hide();
     $( ".is-school" ).show();
+
+    // The happenings of the subject field gets its very own function to maintain a bit of order.
+    subjectSelection('youth');
+  }
+
+  // The subject selection field has alot of choices which should change accordingly.
+  function subjectSelection(choice) {
+    //var myOpts = $(".field-name-field-subjects option").detach();
+    //console.log(myOpts);
+    // Choices reflect the target group field.
+    switch (choice) {
+
+      case 'preschool':
+
+        $(".field-name-field-subjects select").append("<div id='appended'>Hello World</div>");
+        $(".field-name-field-subjects option[value=17]").attach();
+        $(".field-name-field-subjects option[value=22]").attach();
+        $(".field-name-field-subjects option[value=24]").attach();
+        $(".field-name-field-subjects option[value=33]").attach();
+        break;
+
+      case 'primary_school1':
+        $(".field-name-field-subjects option[value=43]").attach();
+        $(".field-name-field-subjects option[value=44]").attach();
+        $(".field-name-field-subjects option[value=46]").attach();
+        $(".field-name-field-subjects option[value=47]").attach();
+        $(".field-name-field-subjects option[value=48]").attach();
+        $(".field-name-field-subjects option[value=49]").attach();
+        $(".field-name-field-subjects option[value=50]").attach();
+        $(".field-name-field-subjects option[value=51]").attach();
+        $(".field-name-field-subjects option[value=52]").attach();
+        $(".field-name-field-subjects option[value=53]").attach();
+        $(".field-name-field-subjects option[value=54]").attach();
+        $(".field-name-field-subjects option[value=56]").attach();
+        $(".field-name-field-subjects option[value=57]").attach();
+        $(".field-name-field-subjects option[value=58]").attach();
+        $(".field-name-field-subjects option[value=59]").attach();
+        $(".field-name-field-subjects option[value=60]").attach();
+        $(".field-name-field-subjects option[value=61]").attach();
+        $(".field-name-field-subjects option[value=71]").attach();
+        break;
+
+      case 'youth1':
+        $(".field-name-field-subjects option[value=72]").attach();
+        $(".field-name-field-subjects option[value=73]").attach();
+        $(".field-name-field-subjects option[value=74]").attach();
+        $(".field-name-field-subjects option[value=75]").attach();
+        $(".field-name-field-subjects option[value=77]").attach();
+        $(".field-name-field-subjects option[value=78]").attach();
+        $(".field-name-field-subjects option[value=79]").attach();
+        $(".field-name-field-subjects option[value=80]").attach();
+        $(".field-name-field-subjects option[value=82]").attach();
+        $(".field-name-field-subjects option[value=84]").attach();
+        $(".field-name-field-subjects option[value=86]").attach();
+        $(".field-name-field-subjects option[value=88]").attach();
+        $(".field-name-field-subjects option[value=89]").attach();
+        $(".field-name-field-subjects option[value=90]").attach();
+        $(".field-name-field-subjects option[value=91]").attach();
+        $(".field-name-field-subjects option[value=92]").attach();
+        $(".field-name-field-subjects option[value=93]").attach();
+        $(".field-name-field-subjects option[value=94]").attach();
+        $(".field-name-field-subjects option[value=95]").attach();
+        $(".field-name-field-subjects option[value=96]").attach();
+        $(".field-name-field-subjects option[value=97]").attach();
+        $(".field-name-field-subjects option[value=98]").attach();
+        $(".field-name-field-subjects option[value=99]").attach();
+        $(".field-name-field-subjects option[value=100]").attach();
+        $(".field-name-field-subjects option[value=101]").attach();
+        $(".field-name-field-subjects option[value=103]").attach();
+        $(".field-name-field-subjects option[value=104]").attach();
+        $(".field-name-field-subjects option[value=105]").attach();
+        $(".field-name-field-subjects option[value=107]").attach();
+        $(".field-name-field-subjects option[value=108]").attach();
+        $(".field-name-field-subjects option[value=109]").attach();
+        $(".field-name-field-subjects option[value=110]").attach();
+        $(".field-name-field-subjects option[value=111]").attach();
+        $(".field-name-field-subjects option[value=112]").attach();
+        $(".field-name-field-subjects option[value=113]").attach();
+        $(".field-name-field-subjects option[value=114]").attach();
+        $(".field-name-field-subjects option[value=115]").attach();
+        $(".field-name-field-subjects option[value=116]").attach();
+        $(".field-name-field-subjects option[value=118]").attach();
+        $(".field-name-field-subjects option[value=119]").attach();
+        $(".field-name-field-subjects option[value=120]").attach();
+        $(".field-name-field-subjects option[value=121]").attach();
+        $(".field-name-field-subjects option[value=122]").attach();
+        $(".field-name-field-subjects option[value=123]").attach();
+        $(".field-name-field-subjects option[value=124]").attach();
+        $(".field-name-field-subjects option[value=125]").attach();
+        $(".field-name-field-subjects option[value=126]").attach();
+        $(".field-name-field-subjects option[value=127]").attach();
+        $(".field-name-field-subjects option[value=129]").attach();
+        break;
+    }
   }
 })(jQuery);
+
+
