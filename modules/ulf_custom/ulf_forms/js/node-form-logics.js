8acca43e322fd358b5d264621752f7cac3851171
Martin Yde <my@aarhus.dk>
2014-03-06 11:37:30 +0100

    Changed target group to checkboxes

--- modules/ulf_custom/ulf_forms/js/node-form-logics.js
+++ modules/ulf_custom/ulf_forms/js/node-form-logics.js
@@ -3,8 +3,43 @@
  * A lot of fields change due to the selection of the target group field.
  */
 (function($) {
+
+
   // Array to hold subgroup checkboxes
   var subgroup = new Array();
+
+  /**
+   * Function to call when the the Target Group selector is changed.
+   * Updates which fields should be shown.
+   */
+  function changeSelection(selected, targetGroupSelections) {
+
+    // Hide all target groups.
+    $(".field-name-field-target-group-sub input").parent().hide()
+
+    // Hide specific fields only relevant for certain target groups.
+    $( ".field-name-field-post-work" ).hide();
+    $( ".field-name-field-educational-material" ).hide();
+    $( ".field-name-field-educational-goals" ).hide();
+    $( ".field-name-field-subjects-primary-school" ).hide();
+    $( ".field-name-field-subjects-youth" ).hide();
+    // Used in field help text.
+    $( ".is-school" ).hide();
+    $( ".field-name-field-inspirational-material" ).hide();
+    $( ".field-name-field-material-suggestions" ).hide();
+
+    // Show stuff depending on what the selection array contains.
+    if ( $.inArray('Dagtilbud ', targetGroupSelections) > -1 ) {
+      selectionPreschool();
+    }
+    if ( $.inArray('Grundskole ', targetGroupSelections) > -1 ) {
+      selectionPrimarySchool();
+    }
+    if ( $.inArray('Ungdomsuddannelse ', targetGroupSelections) > -1 ) {
+      selectionYouth();
+    }
+  };
+
 
   /**
    * Displays and hides Target Subgroup divs according to the accept function.
@@ -19,49 +54,8 @@
       // If the acceptFunction, accepts the text show the input, else hide it.
       if (acceptFunction(text)) {
         $(".field-name-field-target-group-sub input[value=" + value +"]").parent().show();
-      } else {
-        $(".field-name-field-target-group-sub input[value=" + value +"]").parent().hide();
       }
     });
-  };
-
-  /**
-   * Function to call when the the Target Group selector is changed.
-   * Updates which fields should be shown.
-   */
-  function changeSelection() {
-    // Get the text of the target group selector.
-    var selection = $( ".field-name-field-target-group .form-select option:selected").text();
-
-    switch (selection) {
-      // Preschool id.
-      case 'Dagtilbud':
-        selectionPreschool();
-        // Show practical information group.
-        $('#node_course_form_group_practical_information').show();
-        break;
-
-      // School id.
-      case 'Grundskole':
-        selectionPrimarySchool();
-        // Show practical information group.
-        $('#node_course_form_group_practical_information').show();
-        break;
-
-      // Youth education id.
-      case 'Ungdomsuddannelse':
-        // Show practical information group.
-        $('#node_course_form_group_practical_information').show();
-        selectionYouth();
-        break;
-
-      // Default is to show all options.
-      default:
-        displayRelevantSubgroupByKeyword(function() {
-          return true;
-        });
-        break;
-    }
   };
 
   /**
@@ -74,17 +68,10 @@
     });
 
     // Whole fields are either shown or hidden depending on main target group.
-    $( ".field-name-field-post-work" ).hide();
-    $( ".field-name-field-educational-material" ).hide();
     $( ".field-name-field-inspirational-material" ).show();
     $( ".field-name-field-material-suggestions" ).show();
     $( ".field-name-field-educational-goals" ).show();
-    $( ".field-name-field-subjects-primary-school" ).hide();
-    $( ".field-name-field-subjects-youth" ).hide();
-
-    // We act on our own classes as well. This is useful for changing help texts (See purpose field for example).
     $( ".is-preschool" ).show();
-    $( ".is-school" ).hide();
   }
 
   /**
@@ -99,14 +86,7 @@
     // Whole fields are either shown or hidden depending on main target group.
     $( ".field-name-field-post-work" ).show();
     $( ".field-name-field-educational-material" ).show();
-    $( ".field-name-field-inspirational-material" ).hide();
-    $( ".field-name-field-material-suggestions" ).hide();
-    $( ".field-name-field-educational-goals" ).hide();
     $( ".field-name-field-subjects-primary-school" ).show();
-    $( ".field-name-field-subjects-youth" ).hide();
-
-    // We act on our own classes as well. This is useful for changing help texts (See purpose field for example).
-    $( ".is-preschool" ).hide();
     $( ".is-school" ).show();
   }
 
@@ -122,56 +102,33 @@
     // Whole fields are either shown or hidden depending on main target group.
     $( ".field-name-field-post-work" ).show();
     $( ".field-name-field-educational-material" ).show();
-    $( ".field-name-field-inspirational-material" ).hide();
-    $( ".field-name-field-material-suggestions" ).hide();
-    $( ".field-name-field-educational-goals" ).hide();
-    $( ".field-name-field-subjects-primary-school" ).hide();
     $( ".field-name-field-subjects-youth" ).show();
-
-    // We act on our own classes as well. This is useful for changing help texts (See purpose field for example).
-    $( ".is-preschool" ).hide();
     $( ".is-school" ).show();
   }
 
+
+
   // Called when the document has finished loading.
   $(document).ready(function() {
-    // Hide filter selection.
-    $('.filter-wrapper').hide();
 
-    // We add border width in js to reach seven theme.
-    $('#node_course_form_group_practical_information').css('border-width', '2px');
+    /// --- A lot of actions related to click events
 
-    // Hide practical information until target group has been selected.
-    $('#node_course_form_group_practical_information').hide();
+    // Change stuff when a new target group is added or removed.
+    $( ".field-name-field-target-group .form-checkbox" ).click(function(){
+      // The actual clicked item.
+      var checked = ($(this).val());
+      var selected = $(".field-name-field-target-group .form-item-field-target-group-und-" + checked + " label").text();
 
-
-    // Fill the subgroup array.
-    $(".field-name-field-target-group-sub .option").each(function() {
-      var val   = $(this).siblings("input").val();
-      var text  = $(this).text();
-      subgroup.push(new Array(text, val));
+      // An array containing checked of items of the target group field.
+      var targetGroupSelections = new Array();
+      $('.field-name-field-target-group .form-checkbox').each(function () {
+        if (this.checked) {
+          var value = ($(this).val());
+          targetGroupSelections.push($(".field-name-field-target-group .form-item-field-target-group-und-" + value + " label").text());
+        }
+      });
+      changeSelection(selected, targetGroupSelections);
     });
-
-    // When the Target Group selector is changed, change the subgroup options.
-    $('.field-name-field-target-group .form-select').change(changeSelection);
-
-    // Hide help text for preschool as default.
-    $( ".is-preschool" ).hide();
-
-    // Run a check when opening the node form. Used when editing existing nodes.
-    changeSelection();
-
-    // Hide group period as default. This is related to the full year checkbox being checked.
-    if($( ".field-name-field-period-full-year .form-checkbox" ).is(":checked")) {
-      $( "#node_course_form_group_period" ).hide();
-      $( ".field-name-field-period" ).hide();
-    }
-
-    // Hide field collection price if "Free" is checked. This is unchecked as default.
-    if($( ".field-name-field-free .form-checkbox" ).is(":checked")) {
-      $( ".field-name-field-collection-price" ).hide();
-      $( ".field-name-field-price" ).hide();
-    }
 
     // When "Full year" is unchecked show field group for duration.
     $( ".field-name-field-period-full-year .form-checkbox" ).click(function() {
@@ -189,9 +146,11 @@
       if($(this).is(":checked")) {
         $( ".field-name-field-collection-price" ).hide();
         $( ".field-name-field-price" ).hide();
+        $( ".field-name-field-vary-price" ).hide();
       } else {
         $( ".field-name-field-collection-price" ).show();
         $( ".field-name-field-price" ).show();
+        $( ".field-name-field-vary-price" ).show();
       }
     });
 
@@ -220,8 +179,49 @@
         $( ".field-name-field-price .form-text").css('background', '#fff');
       }
     });
-  });
 
+    // When the Target Group selector is changed, change the subgroup options.
+    $('.field-name-field-target-group .form-select').change(changeSelection);
+
+    /// ------ Stuff that happens when the form is first loaded.
+
+    // We add border width in js to reach seven theme.
+    $('#node_course_form_group_practical_information').css('border-width', '2px');
+
+    // Fill the subgroup array.
+    $(".field-name-field-target-group-sub .option").each(function() {
+      var val   = $(this).siblings("input").val();
+      var text  = $(this).text();
+      subgroup.push(new Array(text, val));
+    });
+
+    // Run a check when opening the node form. Used when editing existing nodes.
+    // An array containing checked of items of the target group field.
+    var targetGroupSelections = new Array();
+    $('.field-name-field-target-group .form-checkbox').each(function () {
+      if (this.checked) {
+        var value = ($(this).val());
+        console.log(value);
+        targetGroupSelections.push($(".field-name-field-target-group .form-item-field-target-group-und-" + value + " label").text());
+      }
+    });
+    changeSelection('startup', targetGroupSelections);
+
+    // Hide group period as default. This is related to the full year checkbox being checked.
+    if($( ".field-name-field-period-full-year .form-checkbox" ).is(":checked")) {
+      $( "#node_course_form_group_period" ).hide();
+      $( ".field-name-field-period" ).hide();
+    }
+
+    // Hide field collection price if "Free" is checked. This is unchecked as default.
+    if($( ".field-name-field-free .form-checkbox" ).is(":checked")) {
+      $( ".field-name-field-collection-price" ).hide();
+      $( ".field-name-field-price" ).hide();
+    }
+
+    // Hide filter selection on all fields.
+    $('.filter-wrapper').hide();
+  });
 })(jQuery);
 
 
