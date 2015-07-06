fe0d905e1a59b7249a5f7c6dc95e2a41ebbea8bf
martinyde <yde001@gmail.com>
2015-07-06 09:37:35 +0200

    issue #ULF-76 by martinyde: Added Pages for news and organizers

--- profiles/ulf/themes/ulftheme/template.php
+++ profiles/ulf/themes/ulftheme/template.php
@@ -215,3 +215,63 @@ function ulf_menu_link__menu_about_ulf($variables) {
   $output = l($element ['#title'], $element ['#href'], $element ['#localized_options']);
   return '<li' . drupal_attributes($element ['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
 }
+
+
+/**
+ * Implements theme_item_list().
+ */
+function ulf_item_list($variables) {
+  $items = $variables ['items'];
+  $title = $variables ['title'];
+  $type = $variables ['type'];
+  $attributes = $variables ['attributes'];
+
+  // Only output the list container and title, if there are any list items.
+  // Check to see whether the block title exists before adding a header.
+  // Empty headers are not semantic and present accessibility challenges.
+  $output = '';
+  if (isset($title) && $title !== '') {
+    $output .= '<h3>' . $title . '</h3>';
+  }
+
+  if (!empty($items)) {
+    $output .= "<$type" . drupal_attributes($attributes) . '>';
+    $num_items = count($items);
+    $i = 0;
+    foreach ($items as $item) {
+      $attributes = array();
+      $children = array();
+      $data = '';
+      $i++;
+      if (is_array($item)) {
+        foreach ($item as $key => $value) {
+          if ($key == 'data') {
+            $data = $value;
+          }
+          elseif ($key == 'children') {
+            $children = $value;
+          }
+          else {
+            $attributes [$key] = $value;
+          }
+        }
+      }
+      else {
+        $data = $item;
+      }
+      if (count($children) > 0) {
+        // Render nested list.
+        $data .= theme_item_list(array('items' => $children, 'title' => NULL, 'type' => $type, 'attributes' => $attributes));
+      }
+      if ($i == 1) {
+        $attributes ['class'][] = 'first';
+      }
+      if ($i == $num_items) {
+        $attributes ['class'][] = 'last';
+      }
+      $output .= '<li' . drupal_attributes($attributes) . '>' . $data . "</li>\n";
+    }
+    $output .= "</$type>";
+  }
+  return $output;
+}
