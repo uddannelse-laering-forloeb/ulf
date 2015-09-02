6b8465aa58708bcafbcf98a3781c836ee09c0982
Jesper Kristensen <cableman@linuxdev.dk>
2015-09-02 15:45:38 +0200

    Issue #ULF-168 by cableman: Started on the basic search templates

--- profiles/ulf/modules/ulf_custom/ulf_search/js/search_box_controller.js
+++ profiles/ulf/modules/ulf_custom/ulf_search/js/search_box_controller.js
@@ -6,11 +6,48 @@
  */
 
 /**
- * @TODO: description.
+ * Create filter that pads a number with zero's.
+ *
+ * @TODO: Move this back to the main angular library.
+ */
+angular.module('searchResultApp').filter('trimWordBoundary', function () {
+  "use strict";
+
+  return function (str, len) {
+    // Clean out headlines.
+    str = str.replace(/(<h\d>).+(<\/h\d>)/gi, '');
+
+    // Clean out HTML.
+    str = str.replace(/(<([^>]+)>)/gi, '');
+
+    // Trim string to word boundery.
+    var trimmedString = str.substr(0, len);
+    str = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" ")));
+
+    return str + '...';
+  };
+});
+
+/**
+ * Overrides the default searchBoxApp controller.
  */
 angular.module('searchBoxApp').controller('UlfBoxController', ['CONFIG', 'communicatorService', 'searchProxy', '$scope',
   function (CONFIG, communicatorService, searchProxy, $scope) {
     'use strict';
+
+    function stripAndTrim(str, lenght) {
+      // Clean out headlines.
+      str = str.replace(/(<h\d>).+(<\/h\d>)/gi, '');
+
+      // Clean out HTML.
+      str = str.replace(/(<([^>]+)>)/gi, '');
+
+      // Trim string to word boundery.
+      var trimmedString = str.substr(0, lenght);
+      str = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" ")));
+
+      return str;
+    }
 
     /**
      * Execute the search and emit the results.
@@ -37,6 +74,9 @@ angular.module('searchBoxApp').controller('UlfBoxController', ['CONFIG', 'commun
       );
     }
 
+    /**
+     * Init the contoller.
+     */
     function init() {
       // Get state from pervious searches.
       var state = searchProxy.init();
