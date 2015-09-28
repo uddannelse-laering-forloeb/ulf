983c1f340361c4e9b59ea2b845e239b473d02756
martinyde <yde001@gmail.com>
2015-09-28 11:17:11 +0200

    issue #ULF-176 by martinyde: Added IE9 styles

--- profiles/ulf/themes/ulftheme/templates/html.tpl.php
+++ profiles/ulf/themes/ulftheme/templates/html.tpl.php
@@ -52,7 +52,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no"/>
     <!--<link href="//fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic" rel="stylesheet" type="text/css">-->
-    <!--[if lt IE 9]>
+    <!--[if lt IE 10]>
     <script type="text/javascript" src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <link rel="stylesheet" href="../css/ie9.css" media="all">
     <![endif]-->
