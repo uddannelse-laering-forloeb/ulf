525086fd539eb95aef10d29c9026c5fed0934fc9
martinyde <yde001@gmail.com>
2015-09-30 13:34:04 +0200

    issue #ULF-176 by martinyde: Changed possible files for material

--- profiles/ulf/themes/ulftheme/templates/node--news.tpl.php
+++ profiles/ulf/themes/ulftheme/templates/node--news.tpl.php
@@ -89,7 +89,7 @@
           <?php print render($content['field_image']); ?>
         </div>
         <div class="content--main">
-          <p class="teaser--type">d. <?php print format_date($created, 'dmy'); ?></p>
+          <p class="teaser--type"><?php print format_date($created, 'dmy'); ?></p>
           <?php
           // We hide the comments and links now so that we can render them later.
           hide($content['comments']);
