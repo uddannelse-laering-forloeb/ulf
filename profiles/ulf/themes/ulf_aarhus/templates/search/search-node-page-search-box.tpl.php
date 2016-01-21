d35047909a3eb78be19746295b6786fbe2aee12a
martinyde <yde001@gmail.com>
2016-01-21 13:47:14 +0100

    issue #ULF-336 by martinyde: Removed old module and theme for aarhus

--- profiles/ulf/themes/ulf_aarhus/templates/search/search-node-page-search-box.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * @file
- * Default template for the search box.
- */
-?>
-<div id="searchBoxApp" data-ng-strict-di data-ng-controller="UlfBoxController" data-ng-include="template"></div>
