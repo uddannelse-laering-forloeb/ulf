02da04384e8f4a06a794a3647c6f26438f033936
Martin Yde Granath <my@aarhus.dk>
2016-11-01 12:39:38 +0100

    ULF-514, Changed layout and css

--- profiles/ulf/themes/ulf_default/layouts/front_page/front-page.tpl.php
+++ profiles/ulf/themes/ulf_default/layouts/front_page/front-page.tpl.php
@@ -1,14 +1,10 @@
 <?php
 /**
  * @file
- * Template for a 4 element panel layout.
+ * Template for a 5 element panel layout.
  *
- * This template provides a 4 element layout.
- * This is the template used for displaying courses.
- * Element alpha and gamma get their inner wrapper here. Other elements get their inner wrapper from their element (ie. views, panes etc.)
- *
- * The inner classes are node specific.
- * Alpha and gamma, floats left, beta and delta floats right in an 8-4 zen grid system.
+ * This template provides a 5 element layout.
+ * This is the template used for displaying frontpage.
  *
  * @see ../sass/layout/_layout.scss
  *
@@ -22,44 +18,45 @@
  *   - $content['beta']: Second element
  *   - $content['gamma']: Third element
  *   - $content['delta']: Fourth element
+ *   - $content['epsilon']: Fifth element
  */
 ?>
 <div class="layout-frontpage">
   <div class="layout-inner">
     <?php if(!empty ($content['alpha'])) : ?>
-      <div class="layout-element-alpha">
+      <div class="layout-element-alpha <?php (empty($display->panels['alpha'])) ? print 'is-empty' : ''; ?>">
         <div class="layout-element-alpha-inner">
           <?php print $content['alpha']; ?>
         </div>
       </div>
     <?php endif;?>
     <?php if(!empty ($content['beta'])) : ?>
-      <div class="layout-element-beta">
+      <div class="layout-element-beta <?php (empty($display->panels['beta'])) ? print 'is-empty' : '' ?>">
         <div class="layout-element-beta-inner">
           <?php print $content['beta']; ?>
         </div>
       </div>
     <?php endif;?>
     <?php if(!empty ($content['gamma'])) : ?>
-      <div class="layout-element-gamma">
+      <div class="layout-element-gamma <?php (empty($display->panels['gamma'])) ? print 'is-empty' : '' ?>">
         <div class="layout-element-gamma-inner">
           <?php print $content['gamma']; ?>
         </div>
       </div>
     <?php endif;?>
     <?php if(!empty ($content['delta'])) : ?>
-      <div class="layout-element-delta">
+      <div class="layout-element-delta <?php (empty($display->panels['delta'])) ? print 'is-empty' : '' ?>">
         <div class="layout-element-delta-inner">
           <?php print $content['delta']; ?>
         </div>
       </div>
     <?php endif;?>
-    <div class="layout-element-epsilon">
-      <div class="layout-element-epsilon-inner">
-        <?php if(!empty ($content['epsilon'])) : ?>
+    <?php if(!empty ($content['epsilon'])) : ?>
+      <div class="layout-element-epsilon <?php (empty($display->panels['epsilon'])) ? print 'is-empty' : '' ?>">
+        <div class="layout-element-epsilon-inner">
           <?php print $content['epsilon']; ?>
-        <?php endif;?>
+        </div>
       </div>
-    </div>
+    <?php endif;?>
   </div>
 </div>
