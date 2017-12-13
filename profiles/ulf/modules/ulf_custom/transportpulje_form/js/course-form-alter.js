98c71ee92e3a2a4aa458f2a1e064b452c39e9287
Martin Yde Granath <my@aarhus.dk>
2017-12-13 12:56:05 +0100

    KVF-11: Changed course content type, removed phone

---
+++ profiles/ulf/modules/ulf_custom/transportpulje_form/js/course-form-alter.js
@@ -0,0 +1,42 @@
+/**
+ * Hide transport form if user not editor or course target_group is Youth.
+ * Hide transport form from message form dropdown.
+ **/
+(function ($) {
+  "use strict";
+  Drupal.behaviors.course_form = {
+    attach: function (context, settings) {
+      // Hide if not editor user.
+      if (!inArray('editor', Object.values(settings.user.roles))) {
+        $('.group-transport-app').hide();
+      }
+
+      // Simple inArray function.
+      function inArray(needle, haystack) {
+        var length = haystack.length;
+        for(var i = 0; i < length; i++) {
+          if(haystack[i] == needle) return true;
+        }
+        return false;
+      }
+
+      // Hide on change if youth.
+      $(".field-name-field-target-group input").on( "change", function() {
+        if ($(this).val() == settings.target_group) {
+          $('.group-transport-app').hide();
+        }
+        else {
+          $('.group-transport-app').show();
+        }
+      });
+
+      // Hide on load if youth.
+      if ($(".field-name-field-target-group input:checked").val() == settings.target_group) {
+        $('.group-transport-app').hide();
+      }
+
+      // Hide on load if youth.
+      $('.field-name-field-message-form option[value="2"]').remove();
+    }
+  };
+}(jQuery));
