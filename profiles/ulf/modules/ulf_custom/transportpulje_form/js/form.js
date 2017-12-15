1b36aca7b1846e79d7dbba24df1be0945aae95d7
Martin Yde Granath <my@aarhus.dk>
2017-12-15 09:02:56 +0100

    KVF-11: Changed backend views, and messages

--- profiles/ulf/modules/ulf_custom/transportpulje_form/js/form.js
+++ profiles/ulf/modules/ulf_custom/transportpulje_form/js/form.js
@@ -1,37 +1,21 @@
 (function ($) {
+  "use strict";
   Drupal.behaviors.transportpulje_form = {
     attach: function (context, settings) {
+
       // Load course.
       $('.form-item-course-dropdown').change(function (){
         var value = $('.form-item-course-dropdown .chosen-single span').html();
         var nid = $('.form-item-course-dropdown option').filter(function() { return ($(this).text() === value) }).val();
         $('#course_dropdown_address').load('fetch-address/' + nid);
       });
-
-      // Hide parent elements. We only want to show nested items.
-      $.each(settings.institutions, function(index, value) {
-        if(typeof value.field_tpf_parent_term['und'] !== 'undefined') {
-          var name = value.name;
-          $('.form-item-institution-name option').filter(function(){
-            return $.trim($(this).text()) == value.name;
-          }).remove();
-        }
-      });
-
-      // Remove dash before child items.
-      $('.form-item-institution-name option:not(:first-child)').text(
-        function() {
-          return this.text.replace('-', '');
-        }
-      );
-
+      
       // Setup dropdown lists.
       var inst_by_type = [];
       inst_by_type['school_options'] = '';
       inst_by_type['kindergarten_options'] = '';
       inst_by_type['nursery_options'] = '';
       inst_by_type['daycare_options'] = '';
-
       // Populate dropdown lists.
       $.each(settings.institutions, function(index, value) {
         if(typeof value.field_tpf_type['und'] !== 'undefined') {
@@ -51,12 +35,9 @@
           }
         }
       });
-
       // Change the result of the dropdown based on type selected.
       $('.form-item-institution-type').change(function (){
         var inst_type = $('.form-item-institution-type').find(":selected").text();
-        console.log(inst_type);
-        console.log(inst_by_type['school_options']);
         switch(inst_type) {
           case 'Skole':
             $('.form-item-institution-name #edit-institution-name').html(inst_by_type['school_options']);
