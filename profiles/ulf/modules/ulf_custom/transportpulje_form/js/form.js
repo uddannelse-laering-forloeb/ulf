17c9ac0151468057c1ac61da45e5eaf34c14f1ff
Martin Yde Granath <my@aarhus.dk>
2017-12-19 08:54:10 +0100

    Removed checkbox

--- profiles/ulf/modules/ulf_custom/transportpulje_form/js/form.js
+++ profiles/ulf/modules/ulf_custom/transportpulje_form/js/form.js
@@ -6,34 +6,60 @@
       $('.form-item-course-dropdown').change(function () {
         var value = $('.form-item-course-dropdown .chosen-single span').html();
         var nid = $('.form-item-course-dropdown option').filter(function() { return ($(this).text() === value) }).val();
-        console.log(value);
-        console.log(nid);
         $('#course_dropdown_address').load('fetch-address/' + nid, function () {
           if($(this)[0].innerHTML.length == 0) {
-            $('#edit-course-alt-address').prop('checked', true).trigger('change');
-            $('#edit-course-alt-address').prop("disabled", !this.checked);
+            // When a course without address is selected.
+            $('#edit-course-not-found').prop('checked', false).trigger('change');
+            resetFields()
           }
           else {
-            $('#edit-course-alt-address').prop('checked', false).trigger('change');
-            $('#edit-course-alt-address').removeAttr("disabled");
+            if (nid) {
+              // When a course with address is selected.
+              $('#edit-course-not-found').prop('checked', false).trigger('change');
+              populateFields(this);
+            } else {
+              // When no course (- Select -) selected.
+              $('#edit-course-not-found').prop('checked', true).trigger('change');
+              resetFields()
+            }
           }
         });
       });
 
+      $('#edit-course-not-found').click(function () {
+        if($(this).is(':checked')) {
+          $('.form-item-course-dropdown .chosen-single').html('<span>- Vælg -</span>');
+          $('.form-item-course-dropdown .result-selected').removeClass('result-selected');
+        }
+      });
+
+      // When course is loaded with url parameter. And it's the first page load.
       var url_course_id = urlParam('course_id');
-      if (url_course_id){
+      if (url_course_id && !context[0]){
         $('#course_dropdown_address').load('fetch-address/' + url_course_id, function () {
           if($(this)[0].innerHTML.length == 0) {
-            $('#edit-course-alt-address').prop('checked', true).trigger('change');
-            $('#edit-course-alt-address').prop("disabled", !this.checked);
+            resetFields()
           }
           else {
-            $('#edit-course-alt-address').prop('checked', false).trigger('change');
-            $('#edit-course-alt-address').removeAttr("disabled");
+            populateFields(this);
           }
         });
       }
-      
+
+      // Empty address fields.
+      function resetFields() {
+        $('#edit-street').val('');
+        $('#edit-postal-code').val('');
+        $('#edit-city').val('');
+      }
+
+      // Populate address fields
+      function populateFields(element) {
+        $('#edit-street').val($(element).find('#course_dropdown_street')[0].innerHTML);
+        $('#edit-postal-code').val($(element).find('#course_dropdown_postal')[0].innerHTML);
+        $('#edit-city').val($(element).find('#course_dropdown_city')[0].innerHTML);
+      }
+
       // Setup dropdown lists.
       var inst_by_type = [];
       inst_by_type['school_options'] = '';
@@ -88,6 +114,7 @@
         }
       });
 
+      // Find url parameters
       function urlParam(name){
         var results = new RegExp('[\?&]' + name + '=([^]*)').exec(window.location.href);
         if (results==null){
