95868fbe84fbb04c563d816ac162b8cf32f6e234
Martin Yde Granath <my@aarhus.dk>
2017-12-20 14:37:26 +0100

    KVF-11: Added code comments

--- profiles/ulf/modules/ulf_custom/transportpulje_form/js/form.js
+++ profiles/ulf/modules/ulf_custom/transportpulje_form/js/form.js
@@ -1,8 +1,11 @@
+/**
+ * Modify several aspects of the frontend application form.
+ **/
 (function ($) {
   "use strict";
   Drupal.behaviors.transportpulje_form = {
     attach: function (context, settings) {
-      // Load course.
+      // When the select course dropdownis changed we alter the address fields.
       $('.form-item-course-dropdown').change(function () {
         var value = $('.form-item-course-dropdown .chosen-single span').html();
         var nid = $('.form-item-course-dropdown option').filter(function() { return ($(this).text() === value) }).val();
@@ -34,6 +37,7 @@
         }
       });
 
+      // When no course found checkbox is clicked we alter course dropdown and address fields.
       $('#edit-course-not-found').click(function () {
         if($(this).is(':checked')) {
           // Clear all fields in part one.
@@ -60,20 +64,6 @@
         });
       }
 
-      // Empty address fields.
-      function resetFields() {
-        $('#edit-street').val('');
-        $('#edit-postal-code').val('');
-        $('#edit-city').val('');
-      }
-
-      // Populate address fields
-      function populateFields(element) {
-        $('#edit-street').val($(element).find('#course_dropdown_street')[0].innerHTML);
-        $('#edit-postal-code').val($(element).find('#course_dropdown_postal')[0].innerHTML);
-        $('#edit-city').val($(element).find('#course_dropdown_city')[0].innerHTML);
-      }
-
       // Setup dropdown lists.
       var inst_by_type = [];
       inst_by_type['school_options'] = '';
@@ -81,11 +71,11 @@
       inst_by_type['nursery_options'] = '';
       inst_by_type['daycare_options'] = '';
 
+      // Sort the institution dropdown list.
       var sort_arr = [];
       for (var key in settings.institutions) {
         sort_arr.push(settings.institutions[key]);
       }
-
       var sorted = sort_arr.sort(function (a,b) {
         var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase();
         if (nameA < nameB)
@@ -95,6 +85,7 @@
         return 0;
       });
 
+      // populate institutions dropdown lists.
       $.each(sorted, function(index, value) {
         if(typeof value.field_tpf_type['und'] !== 'undefined') {
           switch(value.field_tpf_type['und']['0']['value']) {
@@ -114,16 +105,14 @@
         }
       });
 
-
-
-      // Remove dash before child items.
+      // Remove dash before child items in institutions dropdown list.
       $('.form-item-institution-name option:not(:first-child)').text(
         function() {
           return this.text.replace('-', '');
         }
       );
 
-      // Change the result of the dropdown based on type selected.
+      // Change the result of the institutions dropdown based on type selected.
       $('.form-item-institution-type').change(function (){
         var inst_type = $('.form-item-institution-type').find(":selected").text();
         switch(inst_type) {
@@ -144,7 +133,21 @@
         }
       });
 
-      // Find url parameters
+      // Empty address fields. Helper function.
+      function resetFields() {
+        $('#edit-street').val('');
+        $('#edit-postal-code').val('');
+        $('#edit-city').val('');
+      }
+
+      // Populate address fields. Helper function.
+      function populateFields(element) {
+        $('#edit-street').val($(element).find('#course_dropdown_street')[0].innerHTML);
+        $('#edit-postal-code').val($(element).find('#course_dropdown_postal')[0].innerHTML);
+        $('#edit-city').val($(element).find('#course_dropdown_city')[0].innerHTML);
+      }
+
+      // Find url parameters helper function.
       function urlParam(name){
         var results = new RegExp('[\?&]' + name + '=([^]*)').exec(window.location.href);
         if (results==null){
