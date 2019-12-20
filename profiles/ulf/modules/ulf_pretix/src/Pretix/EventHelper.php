52b5ab48eb34568399fd7f441caef7b6e08b2732
Peter Vesterkær <pve@novicell.dk>
2019-12-20 08:40:46 +0100

    OS2ULF-109: Changed function call in EventHelper

--- profiles/ulf/modules/ulf_pretix/src/Pretix/EventHelper.php
+++ profiles/ulf/modules/ulf_pretix/src/Pretix/EventHelper.php
@@ -616,7 +616,7 @@ class EventHelper extends AbstractHelper {
         if (module_exists('search_api')) {
           $index = search_api_index_load('courses');
           if (NULL !== $index) {
-            search_api_entity_update($node, 'node');
+            search_api_track_item_change('node', array($node->nid));
             search_api_index_items($index);
           }
         }
